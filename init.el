c846589952479859a8abc00a6ba2a2fc9bf665e7
Lexin Gong <gonglexin@gmail.com>
2016-03-29 22:16:03 +0800

    Font size key binding; Support Ruby, Elixir programming

--- init.el
+++ init.el
@@ -5,14 +5,37 @@
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")))
 
+;;; General Settings
+
+;; Font size
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+
+;;
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; replace buffer-menu with ibuffer
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
 ;;; Enable company-mode
 (add-hook 'after-init-hook 'global-company-mode)
+
+;;; Ruby
+(add-hook 'ruby-mode-hook 'robe-mode)
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
+
+;;; Elixir
+(add-hook 'elixir-mode-hook 'alchemist-mode)
+
 
 ;;; store all backup and autosave files in the tmp dir
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
             `((".*" ,temporary-file-directory t)))
+
+
 
 ;;; For GUI emacs
 (custom-set-variables
