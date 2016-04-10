45059c057f7a1ec2d1896f646cd904b3b12d1887
Lexin Gong <gonglexin@gmail.com>
2016-04-10 22:08:15 +0800

    Config web-mode

--- core/core-programming.el
+++ core/core-programming.el
@@ -2,7 +2,9 @@
 (require 'yasnippet)
 (require 'smartparens-config)
 
-(add-hook 'after-init-hook #'global-flycheck-mode)
+;(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'prog-mode-hook 'flycheck-mode)
+(add-hook 'prog-mode-hook 'smartparens-mode)
 (yas-global-mode 1)
 
 (provide 'core-programming)
--- core/core-web.el
+++ core/core-web.el
@@ -3,6 +3,13 @@
 (require 'smartparens-html)
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 
 ;; make web-mode play nice with smartparens
 (setq web-mode-enable-auto-pairing nil)
