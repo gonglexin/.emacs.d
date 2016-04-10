b72a29fde6f964c77d8e1288897b9fd941b7664b
Lexin Gong <gonglexin@gmail.com>
2016-04-10 20:34:12 +0800

    Config company mode

--- core/core-completion.el
+++ core/core-completion.el
@@ -1,5 +1,10 @@
 (require 'company)
 
+(setq company-idle-delay 0.3)
+(setq company-tooltip-limit 10)
+(setq company-minimum-prefix-length 2)
+(setq company-tooltip-flip-when-above t)
+
 (add-hook 'after-init-hook 'global-company-mode)
 
 (provide 'core-completion)
