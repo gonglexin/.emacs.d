2e01b98092341cd3a081ba2d67ee934b6819e397
Lexin Gong <gonglexin@gmail.com>
2016-04-11 22:40:08 +0800

    Stop inserting encoding comment

--- core/core-ruby.el
+++ core/core-ruby.el
@@ -1,6 +1,7 @@
 (require 'smartparens)
 (require 'projectile-rails)
 
+(setq ruby-insert-encoding-magic-comment nil)
 (add-hook 'ruby-mode-hook #'smartparens-mode)
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
 
